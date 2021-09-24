d61799da1ffc98c53a39ed829675629fdb401328
Joel Feddes <joelfeddes@hotmail.com>
2021-09-24 10:35:46 -0500

    Single button rolls two dice now! Woot woot

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
-function RollSixSidedDie() {
-    roll = Math.floor(Math.random()*6) + 1;
-    roll += Math.floor(Math.random()*6) + 1;
-    roll += Math.floor(Math.random()*6) + 1;
-    document.getElementById("dice").innerHTML = roll;
+function RollSixSidedDice() {
+    roll1 = Math.floor(Math.random()*6) + 1;
+    roll2 = Math.floor(Math.random()*6) + 1
+    document.getElementById("dice").value = roll1
+    document.getElementById("dice2").value = roll2
+}
+
+function CheckMatches(){
+    if (roll1  == roll2) {
+        console.log("Match!");
+        } 
+    else {
+        console.log("No Match :(");
+        }
 }
